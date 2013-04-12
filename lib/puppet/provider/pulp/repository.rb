851332d4bb0d213c64540dc49ee32def28dfb05d
Bart Wouters <bartwouters@bartwouters-cegeka.(none)>
2013-04-12 14:21:16 +0200

    first test to create repo

--- lib/puppet/provider/pulp/repository.rb
+++ lib/puppet/provider/pulp/repository.rb
@@ -1,5 +1,10 @@
 Puppet::Type.type(:pulp).provide(:repository) do
-  
+require 'rubygems'
+require 'net/http'
+require 'net/https'
+require 'uri'
+require 'json'
+
   def exists?
     begin
       #code to check is the repo exists
@@ -9,7 +14,23 @@ Puppet::Type.type(:pulp).provide(:repository) do
   end
 
   def create
-    #code to create a repo 
+@toSend = { 
+            "id" => "tset1",
+}.to_json
+
+
+
+
+    url = URI.parse("https://pulpserver2.example.com/pulp/api/v2/repositories/")
+    req = Net::HTTP::Post.new(url.path, initheader = {'Content-Type' =>'application/json'})
+    req.basic_auth 'test-user', 'test'
+    req.body = "#{@toSend}"
+    sock = Net::HTTP.new(url.host, url.port)
+    sock.use_ssl = true
+    sock.verify_mode = OpenSSL::SSL::VERIFY_NONE
+    res = sock.start {|http| http.request(req) }
+    
+    fail "Couldn't create repo: #{res.code} #{res.body}" unless res.kind_of? Net::HTTPSuccess
   end
 
   def update
