5fa43e47fd9f30da92b69a63fa73b9daec50468b
Bart Wouters <bartwouters@bartwouters-cegeka.(none)>
2013-04-16 12:06:32 +0200

    fixed tabs

--- lib/puppet/type/pulp.rb
+++ lib/puppet/type/pulp.rb
@@ -1,5 +1,5 @@
 Puppet::Type.newtype(:pulp)do
-	@doc = "Interface to manage pulp from within puppet"
+  @doc = "Interface to manage pulp from within puppet"
   
   feature :createable, "The provider can create a repository",
     :methods => [:create]
@@ -10,49 +10,49 @@ Puppet::Type.newtype(:pulp)do
   
   ensurable
 
-	newparam(:repoid, :namevar => true) do
-		desc "Repository id"
-	end
+  newparam(:repoid, :namevar => true) do
+    desc "Repository id"
+  end
 
-	newparam(:displayname) do
-		desc "Repository display name"
-	end
+  newparam(:displayname) do
+    desc "Repository display name"
+  end
 
-	newparam(:description) do
-		desc "Repository description"
-	end
+  newparam(:description) do
+    desc "Repository description"
+  end
 
-	newparam(:onlynewest) do 
-		desc "Only newest version op a given package is downloaded"
-		newvalues(:true, :false)
-	end
+  newparam(:onlynewest) do 
+    desc "Only newest version op a given package is downloaded"
+    newvalues(:true, :false)
+  end
 
   newparam(:repotype) do 
     desc "Descripes witch type of repository you want to create"
     defaultto('rpm_repo')
   end
 
-	newparam(:feed) do 
-		desc "full path to the feed that acts as source "
+  newparam(:feed) do 
+    desc "full path to the feed that acts as source "
     validate do \value\
       unless Pathname.new(value).absolute? ||
-          URI.parse(value).is_a?(URI::HTTP)
+        URI.parse(value).is_a?(URI::HTTP)
         fail("Invalid source #{value}")
-      end
+        end
     end
-	end
+  end
 
   newparam(:feedcacert) do
     desc "full path to the certificate CA to use for authentication"
   end
 
-	newparam(:feedcert) do
-		desc "full path to the certificate to use for authentication when accessing the external feed"
-	end
+  newparam(:feedcert) do
+    desc "full path to the certificate to use for authentication when accessing the external feed"
+  end
 
-	newparam(:feedkey) do
-		desc "full path to the private key for feed_cert"
-	end
+  newparam(:feedkey) do
+    desc "full path to the private key for feed_cert"
+  end
 
   newparam(:hostname) do
     desc "hostname of the pulp-server"
